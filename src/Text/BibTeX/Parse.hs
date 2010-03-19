627bfa333c8b5159ae471073bc1c5e92324077f7
Hening Thielemann <bibtex@henning-thielemann.de>
2010-03-19 11:25:03 +0000

    Parse: comments on parser components

--- src/Text/BibTeX/Parse.hs
+++ src/Text/BibTeX/Parse.hs
@@ -30,11 +30,29 @@ file =
    Parsec.many
       (skippingSpace (fmap Just entry <|> fmap (const Nothing) comment))
 
+{- |
+Parse a line that starts with a hash like
+
+> # this is a comment
+
+.
+-}
 comment :: Parser String
 comment =
    do Parsec.char '#'
       fmap trim $ Parsec.manyTill Parsec.anyChar Parsec.newline
 
+{- |
+Parse a BibTeX entry like
+
+> @article{author2010title,
+>   author = {Firstname Surname},
+>   title = {Title},
+>   year = {2010},
+> }
+
+.
+-}
 entry :: Parser Entry.T
 entry =
    do Parsec.char '@'
@@ -46,7 +64,13 @@ entry =
       skippingSpace (Parsec.char '}')
       return (Entry.Cons entryType bibId assigns)
 
+{- |
+Parse an assignment like
 
+> author = {Firstname Surname}
+
+.
+-}
 assignment :: Parser (String, String)
 assignment =
    do field <- skippingSpace bibIdentifier
@@ -54,16 +78,40 @@ assignment =
       val <- skippingSpace value
       return (field, trim val)
 
+{- |
+Parse an assignment like
+
+> 2010
+
+or
+
+> {Firstname Surname}
+
+or
+
+> "Firstname Surname"
+
+.
+-}
 value :: Parser String
 value =
    Parsec.many1 Parsec.digit <|>
    Parsec.between (Parsec.char '{') (Parsec.char '}') (texSequence '}') <|>
    Parsec.between (Parsec.char '"') (Parsec.char '"') (texSequence '"')
 
+{- |
+Parse a sequence of 'texBlock's until the occurrence of a closing character.
+The closing character is not part of the result.
+-}
 texSequence :: Char -> Parser String
 texSequence closeChar =
    liftM concat (Parsec.many (texBlock closeChar))
 
+{- |
+Parse a single character like @a@,
+a LaTeX macro call like @\\alpha@
+or a block enclosed in curly braces like @{\\\"{a}bc}@.
+-}
 texBlock :: Char -> Parser String
 texBlock closeChar =
    liftM3 (\open body close -> open : body ++ close : [])
@@ -73,12 +121,18 @@ texBlock closeChar =
        Parsec.oneOf "{}'`^&%\".,~# " <|> Parsec.letter] <|>
    fmap (:[]) (Parsec.noneOf [closeChar])
 
+{- |
+Parse a type of a BibTeX entry like @article@.
+-}
 identifier :: Parser String
 identifier =
    liftM2 (:)
       Parsec.letter
       (Parsec.many Parsec.alphaNum)
 
+{- |
+Parse a name of a BibTeX entry like @author2010title@.
+-}
 bibIdentifier :: Parser String
 bibIdentifier =
    liftM2 (:)
